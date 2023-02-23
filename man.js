a3352ea5fa3abff6142a8335188f90821fd1abf7
Felipe Bellas <101675032+felipeBellas@users.noreply.github.com>
2023-02-23 19:07:26 -0300

    Add files via upload

---
+++ man.js
@@ -0,0 +1,13 @@
+
+function pinicial(){
+    window.location.href = 'pinicial.html'
+   }
+   function psomos(){
+    window.location.href = 'psomos.html'
+   }
+   function pclientes(){
+    window.location.href = 'pclientes.html'
+   }
+   function pparcerias(){
+    window.location.href = 'pparcerias.html'
+   }
